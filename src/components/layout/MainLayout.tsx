7b00682ce50c9a0183b4b159b61f1df105dbdb1e
uwem42020-ux <uwem42020@gmail.com>
2026-05-24 04:53:00 +0100

    Add InstallPrompt and ScrollToTop components

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,18 +3,20 @@ import { Outlet } from 'react-router-dom';
 import { Header } from '../common/Header';
 import { Footer } from '../common/Footer';
 import { MobileBottomNav } from '../common/MobileBottomNav';
-import { ScrollToTop } from '../common/ScrollToTop';   // ← NEW import
+import { ScrollToTop } from '../common/ScrollToTop';
+import { InstallPrompt } from '../common/InstallPrompt';
 
 export function MainLayout() {
   return (
     <div className="flex flex-col min-h-screen">
-      <ScrollToTop />   {/* ← NEW: resets scroll position on every route change */}
+      <ScrollToTop />
       <Header />
       <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
       <MobileBottomNav />
+      <InstallPrompt />
     </div>
   );
 }
